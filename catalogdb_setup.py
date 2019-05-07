62cf546508a2b58a3611f9ac32d2537e07a1fab1
Karolina Jimenez <karojg@gmail.com>
2019-05-07 15:22:27 -0600

    DB Setup

---
+++ catalogdb_setup.py
@@ -0,0 +1,58 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+    
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    items = relationship("CategoryItem")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'category_id': self.id,
+            'category_name': self.title,
+            'items': [item.serialize for item in self.items],
+        }
+
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
+
+    title = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category, back_populates='items')
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'title': self.title,
+            'id': self.id,
+            'description': self.description,
+        }
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
